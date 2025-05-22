e907e8cd05e62cb857fe6ce6c03125b205705d39
Arunpranav <arunpranav0803@gmail.com>
2025-05-22 14:33:31 +0000

    git pages

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,12 @@ const withMDX = mdx({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // Needed for static HTML export
+  basePath: "/portfolio", // GitHub Pages subfolder
+  assetPrefix: "/portfolio", // Ensures assets resolve correctly
+  images: {
+    unoptimized: true, // Required for static exports
+  },
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
   sassOptions: {
